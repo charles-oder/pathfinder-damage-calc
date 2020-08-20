5580f2fcb796e1a372376ecf6f25271880699295
CharlesOder <charlie.oder@gmail.com>
2020-08-20 08:43:44 -0500

    added example of value references and lifecycle hooks

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,11 +2,12 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <div>By the way.... {{ booyah }}</div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref, onBeforeMount, onMounted } from 'vue';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 export default defineComponent({
@@ -14,5 +15,16 @@ export default defineComponent({
   components: {
     HelloWorld,
   },
+  setup() {
+    const booyah = ref('monkey')
+    onBeforeMount(() => {
+      console.log('onBeforeMount');
+      booyah.value = 'test';
+    });
+    return {
+      booyah
+    }
+  },
+  
 });
 </script>
