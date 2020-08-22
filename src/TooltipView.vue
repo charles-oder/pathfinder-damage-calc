d1e95a588af2f0525ad8ec563df0c5e569eb895e
CharlesOder <charlie.oder@gmail.com>
2020-08-22 11:19:40 -0500

    made tooltip text more responsive

--- src/TooltipView.vue
+++ src/TooltipView.vue
@@ -1,11 +1,11 @@
 <template>
-        <span class="tooltip">
-            &#9432;<span class="tooltiptext">{{text}}</span>
-        </span>
+    <span class="tooltip-view">
+        &#9432;<span class="tooltiptext">{{text}}</span>
+    </span>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, onMounted } from 'vue';
 
 export default defineComponent({
     name: 'TooltipView',
@@ -13,18 +13,62 @@ export default defineComponent({
     },
     props: {
         text: String
+    },
+    setup() {
+        function positionTooltip(tooltip: HTMLElement) {
+            const tooltipText = tooltip.querySelector('.tooltiptext');
+            if (!(tooltipText instanceof HTMLElement)) {
+                return;
+            }
+            if (!tooltipText) { return; }
+            const tooltipRect = tooltipText.getBoundingClientRect();
+            let tipX = tooltip.getBoundingClientRect().width;
+            let tipY = tooltip.getBoundingClientRect().height;
+            tooltipText.style.top = tipY + 'px';
+            tooltipText.style.left = tipX + 'px';
+
+            const sourceRect = tooltip.getBoundingClientRect();
+
+            if ((sourceRect.x + tooltipRect.width) > window.innerWidth) {
+                tipX = -tooltipRect.width;
+            }
+            if (sourceRect.y + tooltipRect.height >  window.innerHeight) {
+                tipY = -tooltipRect.height;
+            }
+            console.log('x: ' + tipX + ', y: ' + tipY)
+            console.log('source: x: ' + sourceRect.x, ', y: ' + sourceRect.y)
+            if ((sourceRect.x + tipX) < 0) {
+                tipX = -sourceRect.x + 5;
+            }
+            if ((sourceRect.y + tipY) < 0) {
+                tipY = -sourceRect.y + 5;
+            }
+
+            tooltipText.style.top = tipY + 'px';
+            tooltipText.style.left = tipX + 'px';
+        }
+        onMounted(() => {
+            const tooltips = document.querySelectorAll('.tooltip-view');
+            tooltips.forEach((tooltip) => {
+                tooltip.addEventListener('mouseover', () => {
+                    if (tooltip instanceof HTMLElement) {
+                        positionTooltip(tooltip);
+                    }
+                });
+            });
+        });
     }
   
 });
 </script>
 
 <style scoped lang="scss">
-.tooltip {
+.tooltip-view {
   position: relative;
   display: inline-block;
 }
 
-.tooltip .tooltiptext {
+.tooltip-view .tooltiptext {
   visibility: hidden;
   width: 200px;
   max-width: 50vw;
@@ -42,7 +86,7 @@ export default defineComponent({
   z-index: 1;
 }
 
-.tooltip:hover .tooltiptext {
+.tooltip-view:hover .tooltiptext {
   visibility: visible;
 }
 
