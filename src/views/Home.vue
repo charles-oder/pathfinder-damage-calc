376efa81ee957f764c6b29d6fd188259972e4d8d
CharlesOder <charlie.oder@gmail.com>
2020-08-26 13:49:37 -0500

    pulled viewModel class out from Home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,22 @@
 <template>
   <div class="home">
     <div class="settings">
-      <AttackSettingsView v-for="(settings, index) in attackSettings" v-bind:key="settings.name" class="settings-container" v-model:settings="attackSettings[index]"/>
-      <SimSettingsView class="settings-container" v-model:settings="simSettings"/>
-      <button class="calc-button" v-on:click="caclulateClicked()">{{buttonTitle}}</button>
+      <AttackSettingsView v-for="(settings, index) in viewModel.attackSettings" v-bind:key="settings.name" class="settings-container" v-model:settings="viewModel.attackSettings[index]"/>
+      <SimSettingsView class="settings-container" v-model:settings="viewModel.simSettings"/>
+      <button class="calc-button" v-on:click="viewModel.caclulateClicked()">{{viewModel.buttonTitle}}</button>
     </div>
     <div class="results">
-      <AttackResultsView :results="resultSet"/>
+      <AttackResultsView :results="viewModel.resultSet"/>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, onMounted, reactive } from 'vue'
+import { defineComponent, reactive } from 'vue'
 import SimSettingsView from '@/config/SimSettingsView.vue'
 import AttackSettingsView from '@/config/AttackSettingsView.vue'
 import AttackResultsView from '@/attack/AttackResultsView.vue'
-import { FullAttackResultSet } from '@/attack/attack-result'
-import AttackResolver from '@/attack/attack-resolver'
-import SimSettings from '@/config/sim-settings'
-import AttackSettings from '@/config/attack-settings'
+import HomeViewModel from '@/views/home-view-model'
 
 export default defineComponent({
   name: 'Home',
@@ -28,115 +25,11 @@ export default defineComponent({
     AttackSettingsView,
     AttackResultsView
   },
-  data() {
-    return {
-    }
-  },
-  methods: {
-  },
   setup() {
-    const simSettings = reactive(new SimSettings())
-    const attackSettings = reactive([
-      new AttackSettings(),
-      new AttackSettings(),
-      new AttackSettings()
-    ])
-    const results = reactive(new FullAttackResultSet())
-    const resultSet = reactive(new FullAttackResultSet())
-    const buttonTitle = ref('Calculate')
-    let resolvers: Array<AttackResolver> = []
-    let running = false
-    let startTime = 0
-    let lapTime = 0
+    const viewModel = reactive(new HomeViewModel())
 
-    function startTimer() {
-      startTime = new Date().getTime()
-      lapTime = startTime
-    }
-
-    function printTimer(name: string, end = false) {
-        const endTime = new Date().getTime()
-        const runningTime = ((endTime - (end ? startTime : lapTime)) / 1000).toFixed(3)
-        lapTime = endTime
-
-        // eslint-diable-next-line
-        console.log(name + ' completed in ' + runningTime + ' seconds')
-    }
-
-    function killTimeouts() {
-        const highestTimeoutId = setTimeout(";")
-        for (let id = 0 ; id < highestTimeoutId ; id++) {
-            clearTimeout(id)
-        }
-    }
-
-    function runJob(ac: number, iteration: number, batchSize: number) {
-      const totalIterations = parseInt(simSettings.iterations)
-      if (iteration >= totalIterations) {
-        printTimer('AC ' + ac)
-        ac++
-        iteration = 0
-      }
-      if (ac > parseInt(simSettings.acMax)) {
-        printTimer('Job', true)
-        buttonTitle.value = 'Calculate'
-        running = false
-        return
-      }
-      const index = ac - parseInt(simSettings.acMin)
-
-      buttonTitle.value = 'Calculating (AC: ' + ac + ', iteration: ' + iteration + ')... Click to cancel'
-      setTimeout(() => {
-        for (let i = 0; i < batchSize; i++) {
-          for (let r = 0; r < resolvers.length; r++) {
-            resultSet.addResult(index, r, resolvers[r].resolveFullAttack(ac))
-          }
-
-        }
-        runJob(ac, iteration + batchSize, batchSize)
-      }, 0)
-
-    }
-
-    function caclulateClicked() {
-      startTimer()
-      if (running) {
-        running = false
-        killTimeouts()
-        buttonTitle.value = 'Calculate'
-        return
-      }
-      running = true
-      simSettings.save()
-      attackSettings[0].save()
-      resultSet.colors = attackSettings.map((setting) => { return setting.color })
-      resultSet.reset()
-
-      resolvers = attackSettings.map((setting) => { return new AttackResolver(setting) })
-
-      buttonTitle.value = 'Starting run...'
-      const batchSize = Math.max(Math.min(parseInt(simSettings.iterations) / 100, 1000), 1)
-      // eslint-diable-next-line
-      console.log('Starting Run (Batch Size: ' + batchSize + ')')
-      runJob(parseInt(simSettings.acMin), 0, batchSize)
-    }
-    onMounted(() => {
-      attackSettings[0].color = '#BBDB9B'
-      attackSettings[0].name = 'BASE'
-      attackSettings[1].color = '#ABC4A1'
-      attackSettings[1].name = 'OPTION 1'
-      attackSettings[2].color = '#9DB4AB'
-      attackSettings[2].name = 'OPTION 2'
-
-    })
-    
     return {
-      caclulateClicked,
-      simSettings,
-      attackSettings,
-      results,
-      resultSet,
-      buttonTitle
+      viewModel
     }
   },
   
---
+++ src/views/home-view-model.ts
@@ -0,0 +1,101 @@
+import { FullAttackResultSet } from '@/attack/attack-result'
+import AttackResolver from '@/attack/attack-resolver'
+import SimSettings from '@/config/sim-settings'
+import AttackSettings from '@/config/attack-settings'
+
+export default class HomeViewModel {
+    simSettings = new SimSettings()
+    attackSettings = [
+        new AttackSettings(),
+        new AttackSettings(),
+        new AttackSettings()
+    ]
+    resultSet = new FullAttackResultSet()
+    buttonTitle = 'Calculate'
+    resolvers: Array<AttackResolver> = []
+    running = false
+    startTime = 0
+    lapTime = 0
+
+    constructor() {
+        this.attackSettings[0].color = '#BBDB9B'
+        this.attackSettings[0].name = 'BASE'
+        this.attackSettings[1].color = '#ABC4A1'
+        this.attackSettings[1].name = 'OPTION 1'
+        this.attackSettings[2].color = '#9DB4AB'
+        this.attackSettings[2].name = 'OPTION 2'
+      }
+
+    startTimer() {
+        this.startTime = new Date().getTime()
+        this.lapTime = this.startTime
+    }
+
+    printTimer(name: string, end = false) {
+        const endTime = new Date().getTime()
+        const runningTime = ((endTime - (end ? this.startTime : this.lapTime)) / 1000).toFixed(3)
+        this.lapTime = endTime
+
+        // eslint-diable-next-line
+        console.log(name + ' completed in ' + runningTime + ' seconds')
+    }
+
+    killTimeouts() {
+        const highestTimeoutId = setTimeout(";")
+        for (let id = 0; id < highestTimeoutId; id++) {
+            clearTimeout(id)
+        }
+    }
+
+    runJob(ac: number, iteration: number, batchSize: number) {
+        const totalIterations = parseInt(this.simSettings.iterations)
+        if (iteration >= totalIterations) {
+            this.printTimer('AC ' + ac)
+            ac++
+            iteration = 0
+        }
+        if (ac > parseInt(this.simSettings.acMax)) {
+            this.printTimer('Job', true)
+            this.buttonTitle = 'Calculate'
+            this.running = false
+            return
+        }
+        const index = ac - parseInt(this.simSettings.acMin)
+
+        this.buttonTitle = 'Calculating (AC: ' + ac + ', iteration: ' + iteration + ')... Click to cancel'
+        setTimeout(() => {
+            for (let i = 0; i < batchSize; i++) {
+                for (let r = 0; r < this.resolvers.length; r++) {
+                    this.resultSet.addResult(index, r, this.resolvers[r].resolveFullAttack(ac))
+                }
+
+            }
+            this.runJob(ac, iteration + batchSize, batchSize)
+        }, 0)
+
+    }
+
+    caclulateClicked() {
+        this.startTimer()
+        if (this.running) {
+            this.running = false
+            this.killTimeouts()
+            this.buttonTitle = 'Calculate'
+            return
+        }
+        this.running = true
+        this.simSettings.save()
+        this.attackSettings[0].save()
+        this.resultSet.colors = this.attackSettings.map((setting) => { return setting.color })
+        this.resultSet.reset()
+
+        this.resolvers = this.attackSettings.map((setting) => { return new AttackResolver(setting) })
+
+        this.buttonTitle = 'Starting run...'
+        const batchSize = Math.max(Math.min(parseInt(this.simSettings.iterations) / 100, 1000), 1)
+        // eslint-diable-next-line
+        console.log('Starting Run (Batch Size: ' + batchSize + ')')
+        this.runJob(parseInt(this.simSettings.acMin), 0, batchSize)
+    }
+
+}
